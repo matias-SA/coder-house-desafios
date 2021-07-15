606d4d9956fe55d89940f89bc8ffa2b6facd70bc
matias-SA <maatias.martin@hotmail.com>
2021-07-15 19:03:34 -0300

    subo desafio 10

---
+++ 10. Motores de Plantillas/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const product = require('./api/product');
+const handlebars = require('express-handlebars')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let routerApi = express.Router()
+
+app.use('/guardar-producto', express.static(__dirname + '/public'))
+
+app.engine(
+    "hbs",
+    handlebars({
+        extname: ".hbs",
+        defaultLayout: 'index.hbs',
+    })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.get('/', (req, res) => {
+    res.render('vista', { products: product.getProductList, isListEmpty: product.isListEmpty() })
+})
+
+routerApi.get('/productos', (req, res) => {
+    res.send(product.getProductList)
+});
+
+routerApi.get('/productos/:id', (req, res) => {
+    res.send(product.getProduct(req.params.id))
+});
+
+routerApi.post('/productos/guardar', (req, res) => {
+    res.redirect('/guardar-producto')
+    res.send(product.newProduct(req.body))
+})
+
+routerApi.put('/productos/actualizar/:id', (req, res) => {
+    res.send(product.postProduct(req.params.id, req.body))
+})
+
+routerApi.delete('/productos/borrar/:id', (req, res) => {
+    res.send(product.deleteProduct(req.params.id))
+})
+
+app.use('/api', routerApi)
+
+const puerto = 8080;
+
+const server = app.listen(puerto, () => {
+    console.log(`servidor escuchando en http://localhost:${puerto}`);
+});
+
+server.on('error', error => {
+    console.log('error en el servidor:', error);
+});
