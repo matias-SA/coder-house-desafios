778771e282ea5bb8ddca49c3d075fccf05306d63
matias-SA <maatias.martin@hotmail.com>
2021-07-21 19:08:57 -0300

    Agrego el desafio numero 11

---
+++ 11. Motores de Plantillas PT2/ejs/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const product = require('./api/product');
+const handlebars = require('express-handlebars')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let routerApi = express.Router()
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get('/', (req, res) => {
+    res.render("vista.ejs", { isProduct: product.isListEmpty(), products: product.productList });
+})
+app.get('/guardar-producto', (req, res) => {
+    res.render("formulario.ejs");
+})
+
+
+routerApi.get('/productos', (req, res) => {
+    res.send(product.getProductList)
+});
+
+routerApi.get('/productos/:id', (req, res) => {
+    res.send(product.getProduct(req.params.id))
+});
+
+routerApi.post('/productos/guardar', async (req, res) => {
+    await product.newProduct(req.body)
+    res.redirect('/guardar-producto')
+})
+
+routerApi.put('/productos/actualizar/:id', (req, res) => {
+    res.send(product.postProduct(req.params.id, req.body))
+})
+
+routerApi.delete('/productos/borrar/:id', (req, res) => {
+    res.send(product.deleteProduct(req.params.id))
+})
+
+app.use('/api', routerApi)
+
+const puerto = 8080;
+
+const server = app.listen(puerto, () => {
+    console.log(`servidor escuchando en http://localhost:${puerto}`);
+});
+
+server.on('error', error => {
+    console.log('error en el servidor:', error);
+});
---
+++ 11. Motores de Plantillas PT2/pug/api/product.js
@@ -0,0 +1,54 @@
+class Product {
+    constructor(title, price, thumbnail) {
+        this.title = title,
+            this.price = price,
+            this.thumbnail = thumbnail
+    }
+    productList = [];
+    id = 0;
+
+    newProduct(dataProduct) {
+        let productsLength = this.productList.length
+        let newProduct = {
+            title: dataProduct.title,
+            price: dataProduct.price,
+            thumbnail: dataProduct.thumbnail,
+            id: this.id += 1
+        }
+        this.productList.push(newProduct);
+        return newProduct
+    }
+
+    get getProductList() {
+        let productsLength = this.productList.length
+        if (productsLength == 0) {
+            return { error: 'no hay productos cargados' }
+        }
+        return this.productList
+    }
+    getProduct(id) {
+        let productFound = this.productList.find(product => product.id == id)
+        if (productFound == undefined) {
+            return { error: 'producto no encontrado' }
+        }
+        return productFound
+    }
+    postProduct(idData, dataProduct) {
+        let id = parseInt(idData)
+        let indexProduct = this.productList.findIndex(product => product.id === id);
+        let { title, price, thumbnail } = dataProduct
+        this.productList.splice(indexProduct, 1, { title, price, thumbnail, id })
+        return { title, price, thumbnail, id }
+    }
+
+    deleteProduct(idData) {
+        let id = parseInt(idData)
+        let productRemoved = this.getProduct(id);
+        let indexProduct = this.productList.findIndex(product => product.id === id);
+        return indexProduct == -1 ? { error: "No existe el producto que desea borrar" } : this.productList.splice(indexProduct, 1)
+    }
+    isListEmpty() {
+        return this.productList.length > 0 ? true : false;
+    }
+}
+module.exports = new Product;
