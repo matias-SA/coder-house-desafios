a09d55fd03f155d7da8c80e78a96dfdec9de791b
matias-SA <maatias.martin@hotmail.com>
2021-08-19 14:10:10 -0300

    Refactorizaciones desafio 15

--- 15. Primera base de datos/items.sql
+++ 15. Primera base de datos/items.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-08-2021 a las 03:14:37
--- Versión del servidor: 10.4.20-MariaDB
--- Versión de PHP: 8.0.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `prueba`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `items`
---
 
 CREATE TABLE `items` (
   `nombre` varchar(11) NOT NULL,
@@ -34,35 +10,28 @@ CREATE TABLE `items` (
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `items`
---
 
-INSERT INTO `items` (`nombre`, `categoria`, `stock`, `id`) VALUES
-('Leche', 'Lacteos', 45, 2),
-('Crema', 'Lacteos', 15, 3);
+INSERT INTO `items` (`nombre`, `categoria`, `stock`) VALUES
+('Fideos', 'Harina', 20),
+('Leche', 'Lacteos', 30),
+('Crema', 'Lacteos', 15);
 
---
--- Índices para tablas volcadas
---
+/* Listar los registros agregados */
+SELECT * FROM items
 
---
--- Indices de la tabla `items`
---
+/* Borrar el item con id = 1 */
+DELETE FROM items WHERE id = 1
+
+/* Actualizar el stock del item con id = 2 a 45 */
+UPDATE items SET stock = 45 WHERE id = 2
+
+/* Listar los registros comprobando que los datos estén actualizados según las acciones
+realizadas */
+SELECT * FROM items
+
 ALTER TABLE `items`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `items`
---
 ALTER TABLE `items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
